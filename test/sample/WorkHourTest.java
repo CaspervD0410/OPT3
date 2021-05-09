14c7ead0c0af5e3bd0580ed1d3f392d2e14d0040
CaspervD0410 <Casper@vdavis.local>
2021-05-09 11:01:42 +0200

    Update WorkHourTest.java

--- test/sample/WorkHourTest.java
+++ test/sample/WorkHourTest.java
@@ -9,13 +9,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class WorkHourTest {
     Employee emp1 = new Employee();
-    Client Plus = new Client();
+    WorkHour hour = new WorkHour();
 
     @Test
     public void SaveWorkHour() {
-        assertTrue(WorkHour.saveWorkHour("06-05-2021", "10:00", "15:00", Plus, emp1, "Werk"));
-        assertFalse(WorkHour.saveWorkHour("06-05-2021", "10:00", "15:00", "glopr", emp1, "Werk"));
-        assertFalse(WorkHour.saveWorkHour("05-14-2028", "10:00", "15:00", Plus, emp1, "Werk"));
-        assertFalse(WorkHour.saveWorkHour("06-05-2021", "10:00", "08:00", Plus, emp1, "Werk"));
+        assertTrue(hour.saveWorkHour("06-05-2021", "10:00", "15:00", "Plus", emp1, "Werk"));
+        assertFalse(hour.saveWorkHour("06-05-2021", "10:00", "15:00", "glopr", emp1, "Werk"));
+        assertFalse(hour.saveWorkHour("05-14-2028", "10:00", "15:00", "Plus", emp1, "Werk"));
+        assertFalse(hour.saveWorkHour("06-05-2021", "10:00", "08:00", "Plus", emp1, "Werk"));
     }
 }
