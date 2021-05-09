91cd1470bd73c05c81da9cf11398fbb76a553eb7
CaspervD0410 <Casper@vdavis.local>
2021-05-09 09:23:33 +0200

    Klassen aanmaken

---
+++ src/sample/Employee.java
@@ -0,0 +1,11 @@
+package sample;
+
+import java.util.ArrayList;
+
+public class Employee {
+    private ArrayList<WorkHour> workHours = new ArrayList<>();
+    private String name;
+    private String funtion;
+
+
+}
---
+++ src/sample/WorkHour.java
@@ -0,0 +1,13 @@
+package sample;
+
+import java.time.*;
+
+public class WorkHour {
+    private LocalDate date;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private Client client;
+    private Employee employee;
+    private String description;
+
+}
