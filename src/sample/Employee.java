08a0594a06619d0576f56269fbc45ac2553caa5c
CaspervD0410 <Casper@vdavis.local>
2021-05-19 23:43:32 +0200

    Edit classes from UML

--- src/sample/Employee.java
+++ src/sample/Employee.java
@@ -29,6 +29,10 @@ public class Employee {
         return name;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
     public String getFuntion() {
         return funtion;
     }
--- src/sample/Login.java
+++ src/sample/Login.java
@@ -1,10 +1,45 @@
 package sample;
 
+import java.util.Scanner;
+
 public class Login {
-    private Login singleton;
+    private static Login singleton;
     private Employee loggedInUser;
 
     private Login() {
         loggedInUser=null;
+    }
+    public static Login getInstance() {
+        if(singleton==null) {
+            singleton = new Login();
+        }
+        return singleton;
+    }
+    private boolean userExists (String name) {
+        for (Employee emp : Employee.employees) {
+            if (emp.getName().equals(name)) {
+                loggedInUser=emp;
+                return true;
+            }
+        }
+        return false;
+    }
+    private boolean userIsAuthenticated() { return loggedInUser != null; }
+    public boolean isAuthenticated() {
+        if (userIsAuthenticated()) {
+            return true;
+        }
+        else {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Voer uw gebruikersnaam in: ");
+            String userName = in.nextLine();
+            System.out.println("Voer uw wachtwoord in: ");
+            String password = in.nextLine();
+            if (userExists(userName)&&password.equals(loggedInUser.getPassword())) {
+                return true;
+            }
+            System.out.println("Gebruikersnaam of wachtwoord klopt niet.");
+            return false;
+        }
     }
 }
